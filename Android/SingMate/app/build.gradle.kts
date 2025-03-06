330f6468daed39b6a6560b81613a0fc8f0187a01
Mosekim <kimmose1992@gmail.com>
2025-03-06 21:16:35 +0900

    ADD firebase SDK

--- Android/SingMate/app/build.gradle.kts
+++ Android/SingMate/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.googleServices) apply false
 }
 
 android {
@@ -42,6 +43,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(platform(libs.firebaseBom))
+    implementation(libs.firebaseAnalytics)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
