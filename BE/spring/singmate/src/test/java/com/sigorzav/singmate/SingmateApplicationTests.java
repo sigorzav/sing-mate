b30a02c9eedcf6f8019bc2febcc97d6bd0cb786d
Mosekim <kimmose1992@gmail.com>
2025-03-07 20:17:25 +0900

    SpringBoot singmate ADD

---
+++ BE/spring/singmate/src/test/java/com/sigorzav/singmate/SingmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigorzav.singmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
