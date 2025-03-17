3f2930fb79c3ddfbd1c1115f1d9d86fdff13b67c
Mosekim <kimmose1992@gmail.com>
2025-03-17 15:24:07 +0900

    feat: [Spring] - Security 모든 요청 임시 허용)

--- BE/spring/singmate/src/main/java/com/sigorzav/singmate/config/SecurityConfig.java
+++ BE/spring/singmate/src/main/java/com/sigorzav/singmate/config/SecurityConfig.java
@@ -1,4 +1,20 @@
 package com.sigorzav.singmate.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // 모든 요청 허용
+                .csrf(AbstractHttpConfigurer::disable); // CSRF 비활성화
+
+        return http.build();
+    }
 }
