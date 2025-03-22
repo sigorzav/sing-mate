9b66ed87926a7c18dadcb7697b511ce6bd7b93b9
Mosekim <kimmose1992@gmail.com>
2025-03-22 15:48:54 +0900

    feat: [Spring] - Redis, modelMapper Config 추가

---
+++ BE/spring/singmate/src/main/java/com/sigorzav/singmate/config/RedisConfig.java
@@ -0,0 +1,41 @@
+package com.sigorzav.singmate.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    // RedisTemplate 설정
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        // Serialization 설정
+        redisTemplate.setKeySerializer(new StringRedisSerializer());                // Key 직렬화
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // Value 직렬화
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
