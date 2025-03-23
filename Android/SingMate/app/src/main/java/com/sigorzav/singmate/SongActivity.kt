db57d36174ee6b2b94e448736494a23238f50b68
Mosekim <kimmose1992@gmail.com>
2025-03-23 20:58:15 +0900

    refactor: [Android] - SongActivity:노래 검색 미사용 라이브러리 정리

--- Android/SingMate/app/src/main/java/com/sigorzav/singmate/SongActivity.kt
+++ Android/SingMate/app/src/main/java/com/sigorzav/singmate/SongActivity.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.sigorzav.singmate.ui.song.SongSearchScreen
-
 import com.sigorzav.singmate.viewmodel.song.SongSearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
